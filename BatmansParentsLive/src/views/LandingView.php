4ee073b254b389f60c0b35f34e4092dfc3c3e0b9
Janice Lu <jjlu555@gmail.com>
2020-11-11 20:59:59 -0800

    Update LandingView.php
    - add file upload form

--- BatmansParentsLive/src/views/LandingView.php
+++ BatmansParentsLive/src/views/LandingView.php
@@ -9,5 +9,11 @@ class LandingView extends View
   public function render(){
     $header = new header();
     $header->render();
+    ?>
+      <form class="addimage" action="./index.php?c=addImage" method="post" enctype="multipart/form-data">
+        <label for="imageToUpload">New Image: <input type="text" id="imageToUpload"></label>
+        <label for="submitImage"><input type="submit" value="Upload" name="submit"></label>
+      </form>
+    <?php
   }
 }
