57137e56eb4b7ec4d367b2900ee6aec09e5dabd8
Janice Lu <jjlu555@gmail.com>
2020-11-21 21:05:13 -0800

    add upload functionality pt2
    - didnt add these the first time around

--- BatmansParentsLive/hw4/src/controllers/UploadAdapter.php
+++ BatmansParentsLive/hw4/src/controllers/UploadAdapter.php
@@ -23,14 +23,25 @@ class UploadAdapter extends Adapter
           $uploadFile = $_FILES["imageToUpload"];
           $uploadName = $_FILES["imageToUpload"]["name"];
           $this->logger->notice("Attempting to upload " . $uploadName);
-          $validateResults = $this->uploadModel->checkImage($this->logger, $uploadFile);
-          if(!$validateResults)
-          {
+          //check image to see if it meets requirements
+          $validateResults = $this->uploadModel->checkImage($this->logger);
+          if(!$validateResults) {
             $this->logger->error("Attempt to upload failed. Did not meet upload requirements.");
           }
-          else
-          {
-            $this->logger->notice("Attempt to upload success. Did not meet upload requirements.");
+          else {
+            //upload image to server after verifying file meets requirements
+            if($this->uploadModel->uploadImage($this->logger)) {
+              //change image to be 360x360
+              if($this->uploadModel->convertImage($this->logger)) {
+                $this->logger->notice("Attempt to upload successful.");
+              }
+              else {
+                $this->logger->error("Attempt to change file dimensions unsuccessful.");
+              }
+            }
+            else {
+              $this->logger->error("Attempt to upload unsuccessful.");
+            }
           }
         }
         else {
--- BatmansParentsLive/hw4/src/models/UploadTools.php
+++ BatmansParentsLive/hw4/src/models/UploadTools.php
@@ -4,12 +4,12 @@ namespace BatmansParentsLive\hw4\Models;
 
 class UploadTools
 {
-  function checkImage($logger, $image)
+  private $directory = "hw4/src/resources/";
+  function checkImage($logger)
   {
-    $directory = "./hw4/src/resources/";
-    $file = $directory . basename($_FILES["imageToUpload"]["name"]);
-    $fileType = strtolower(pathinfo($file,PATHINFO_EXTENSION));
-    $uploadPass = 1;
+
+    $file = $this->directory . basename($_FILES["imageToUpload"]["name"]);
+    $fileType = strtolower(pathinfo($file, PATHINFO_EXTENSION));
     $returnMsg = "";
     if (isset($_POST["submit"]))
     {
@@ -25,5 +25,54 @@ class UploadTools
       }
       return 1;
     }
+  }
+
+  function uploadImage($logger){
+    $upload_destination = "./" . $this->directory . basename($_FILES["imageToUpload"]["name"]);
+    echo $upload_destination;
+    $fileType = strtolower(pathinfo($upload_destination, PATHINFO_EXTENSION));
+    if(isset($_POST["submit"]))
+    {
+      $check = getimagesize($_FILES["imageToUpload"]["tmp_name"]);
+      if($check == false)
+      {
+        $logger->error("File is not an image. Upload failed");
+        return 0;
+      }
+      else
+      {
+        if(move_uploaded_file($_FILES["imageToUpload"]["tmp_name"], $upload_destination))
+        {
+          $newImageName = "./hw4/src/resources/active_image.jpg";
+          if(rename($upload_destination, $newImageName))
+          {
+            return 1;
+          }
+          else {
+            $logger->error("Error changing file name.");
+            return 0;
+          }
+        }
+        else
+        {
+          $logger->error("Error uploading file.");
+          return 0;
+        }
+      }
+    }
+
+
+
+  }
+
+  function convertImage(){
+    $dimensions = 360; // 360px as per Requirements
+    $filePath = realpath($_FILES["imageToUpload"]["tmp_name"]);
+    list($origWidth, $origHeight) = getimagesize($filePath);
+    $source = imagecreatefromjpeg($_FILES["imageToUpload"]["name"]);
+    $destination = imagecreatetruecolor($dimensions, $dimensions);
+
+    imagecopyresized($destination, $source, 0, 0, 0, 0, $dimensions, $dimensions, $origWidth, $origHeight);
+    imagejpeg($output);
   }
 }
